8bdce7f4b474240429e38cd72a059268e06aecb2
nikolasguillen <65493479+nikolasguillen@users.noreply.github.com>
2022-05-12 19:15:46 +0200

    aggiunti colori dinamici alla navbar (in tono con l'eventuale bottomNavigation)

--- app/src/main/java/com/example/eplan/presentation/MainActivity.kt
+++ app/src/main/java/com/example/eplan/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.eplan.presentation
 import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -10,22 +11,29 @@ import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.animation.*
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.example.eplan.presentation.navigation.BottomNavBarItems
 import com.example.eplan.presentation.navigation.NavGraph
+import com.example.eplan.presentation.navigation.Screen
 import com.example.eplan.presentation.ui.components.BottomNavBar
 import com.example.eplan.presentation.ui.theme.AppTheme
+import com.example.eplan.presentation.util.TAG
 import com.example.eplan.presentation.util.THEME_STATE_KEY
 import com.example.eplan.presentation.util.getCurrentRoute
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.google.android.material.color.DynamicColors
+import com.google.android.material.elevation.SurfaceColors
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -44,27 +52,46 @@ class MainActivity : AppCompatActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
-
-            val mode = sharedPreferences.getInt(THEME_STATE_KEY, AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-            AppCompatDelegate.setDefaultNightMode(mode)
-
-            val navController = rememberNavController()
-            val systemUiController = rememberSystemUiController()
-            val useDarkIcons = !isSystemInDarkTheme()
-            var bottomBarState by rememberSaveable { mutableStateOf(false) }
-
-            SideEffect {
-                systemUiController.setSystemBarsColor(
-                    color = Color.Transparent,
-                    darkIcons = useDarkIcons
-                )
-                systemUiController.setNavigationBarColor(
-                    color = Color.Transparent,
-                    darkIcons = useDarkIcons
-                )
-            }
-
             AppTheme {
+                val mode = sharedPreferences.getInt(
+                    THEME_STATE_KEY,
+                    AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                )
+                AppCompatDelegate.setDefaultNightMode(mode)
+
+                val navController = rememberNavController()
+                val currentRoute = getCurrentRoute(navController = navController)
+                val systemUiController = rememberSystemUiController()
+                val useDarkIcons = !isSystemInDarkTheme()
+                var bottomBarState by rememberSaveable { mutableStateOf(false) }
+                DynamicColors.applyToActivityIfAvailable(this)
+
+                SideEffect {
+                    systemUiController.setStatusBarColor(
+                        color = Color.Transparent,
+                        darkIcons = useDarkIcons
+                    )
+                }
+
+                currentRoute?.let {
+                    with(it) {
+                        when {
+                            contains("list", true) -> {
+                                window.navigationBarColor =
+                                    SurfaceColors.SURFACE_2.getColor(this@MainActivity)
+                            }
+                            contains("details", true) -> {
+                                window.navigationBarColor =
+                                    MaterialTheme.colorScheme.primary.toArgb()
+                            }
+                            else -> {
+                                window.navigationBarColor =
+                                    MaterialTheme.colorScheme.surface.toArgb()
+                            }
+                        }
+                    }
+                }
+
                 Scaffold(
                     modifier = Modifier.navigationBarsPadding(),
                     bottomBar = {
@@ -74,7 +101,7 @@ class MainActivity : AppCompatActivity() {
                             BottomNavBarItems.Appointments
                         )
 
-                        bottomBarState = when (getCurrentRoute(navController = navController)) {
+                        bottomBarState = when (currentRoute) {
                             (BottomNavBarItems.Home.route) -> {
                                 true
                             }
