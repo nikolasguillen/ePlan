89b86af4c6d695aac2b60310717852fa4b270971
nikolasguillen <65493479+nikolasguillen@users.noreply.github.com>
2022-05-19 15:19:47 +0200

    aggiornate dipendenze

--- app/src/main/java/com/example/eplan/presentation/MainActivity.kt
+++ app/src/main/java/com/example/eplan/presentation/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.eplan.presentation
 import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -11,12 +10,14 @@ import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.animation.*
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.*
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,10 +26,8 @@ import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.example.eplan.presentation.navigation.BottomNavBarItems
 import com.example.eplan.presentation.navigation.NavGraph
-import com.example.eplan.presentation.navigation.Screen
 import com.example.eplan.presentation.ui.components.BottomNavBar
 import com.example.eplan.presentation.ui.theme.AppTheme
-import com.example.eplan.presentation.util.TAG
 import com.example.eplan.presentation.util.THEME_STATE_KEY
 import com.example.eplan.presentation.util.getCurrentRoute
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
