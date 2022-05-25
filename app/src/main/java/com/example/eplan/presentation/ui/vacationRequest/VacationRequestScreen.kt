1e1c706f7c8abd76b3c3d863935877875ff8e080
Nikolas <nikolas8991@gmail.com>
2022-05-25 22:26:06 +0200

    aggiunto selettore tipo ferie in VacationRequestScreen.kt (non finita)

--- app/src/main/java/com/example/eplan/presentation/ui/vacationRequest/VacationRequestScreen.kt
+++ app/src/main/java/com/example/eplan/presentation/ui/vacationRequest/VacationRequestScreen.kt
@@ -1,31 +1,32 @@
 package com.example.eplan.presentation.ui.vacationRequest
 
-import android.widget.CalendarView
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.eplan.presentation.navigation.BottomNavBarItems
 import com.example.eplan.presentation.ui.components.BottomSingleActionBar
+import com.example.eplan.presentation.ui.components.CustomDateButton
+import com.example.eplan.presentation.util.fromDateToLocalDate
 import com.example.eplan.presentation.util.spacing
-import java.time.Instant
-import java.util.*
 
 @ExperimentalMaterial3Api
 @Composable
 fun VacationRequestScreen(
     viewModel: VacationRequestViewModel
 ) {
-
     Scaffold(
         topBar = {
             SmallTopAppBar(title = { Text(text = "Richiesta ferie") }, navigationIcon = {
@@ -42,56 +43,87 @@ fun VacationRequestScreen(
         }) { paddingValues ->
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
+            verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.medium),
             modifier = Modifier
                 .padding(paddingValues)
+                .padding(MaterialTheme.spacing.medium)
                 .fillMaxWidth()
         ) {
-            Row() {
-                Text(
-                    text = "Scegli la data iniziale",
-                    style = MaterialTheme.typography.headlineLarge
-                )
-            }
-            AndroidView(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = MaterialTheme.spacing.medium),
-                factory = {
-                    CalendarView(it).apply {
-                        this.date = Date.from(Instant.now()).time
-                    }
-                })
-            Row() {
-                FilledTonalButton(onClick = { /*TODO*/ }) {
-                    Text(text = "Conferma data selezionata")
-                }
-            }
-            Row(
-                horizontalArrangement = Arrangement.SpaceAround,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = MaterialTheme.spacing.medium)
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.extraSmall)
             ) {
-                AnimatedVisibility(
-                    visible = viewModel.startDate.value != null,
-                    enter = fadeIn(),
-                    exit = fadeOut()
-                ) {
-                    Column() {
-                        Text(text = "Dal", style = MaterialTheme.typography.headlineMedium)
+                Text(text = "Tipo di richiesta", style = MaterialTheme.typography.labelLarge)
+                Row(horizontalArrangement = Arrangement.spacedBy((-1).dp)) {
+                    Surface(
+                        color = if (viewModel.singleDayVacancy) MaterialTheme.colorScheme.secondaryContainer else MaterialTheme.colorScheme.surface,
+                        shape = RoundedCornerShape(
+                            topStartPercent = 50,
+                            bottomStartPercent = 50,
+                            topEndPercent = 0,
+                            bottomEndPercent = 0
+                        ),
+                        border = BorderStroke(1.dp, MaterialTheme.colorScheme.onSurface),
+                        modifier = Modifier
+                            .clip(
+                                RoundedCornerShape(
+                                    topStartPercent = 50,
+                                    bottomStartPercent = 50,
+                                    topEndPercent = 0,
+                                    bottomEndPercent = 0
+                                )
+                            )
+                            .clickable {
+                                viewModel.singleDayVacancy = !viewModel.singleDayVacancy
+                            }
+                    ) {
+                        Text(
+                            text = "Giornata",
+                            style = MaterialTheme.typography.labelLarge,
+                            modifier = Modifier.padding(MaterialTheme.spacing.medium)
+                        )
                     }
-                }
-                AnimatedVisibility(
-                    visible = viewModel.endDate.value != null,
-                    enter = fadeIn(),
-                    exit = fadeOut()
-                ) {
-                    Column() {
-                        Text(text = "Al", style = MaterialTheme.typography.headlineMedium)
+                    Surface(
+                        color = if (!viewModel.singleDayVacancy) MaterialTheme.colorScheme.secondaryContainer else MaterialTheme.colorScheme.surface,
+                        shape = RoundedCornerShape(
+                            topStartPercent = 0,
+                            bottomStartPercent = 0,
+                            topEndPercent = 50,
+                            bottomEndPercent = 50
+                        ),
+                        border = BorderStroke(1.dp, MaterialTheme.colorScheme.onSurface),
+                        modifier = Modifier
+                            .clip(
+                                RoundedCornerShape(
+                                    topStartPercent = 0,
+                                    bottomStartPercent = 0,
+                                    topEndPercent = 50,
+                                    bottomEndPercent = 50
+                                )
+                            )
+                            .clickable {
+                                viewModel.singleDayVacancy = !viewModel.singleDayVacancy
+                            }
+                    ) {
+                        Text(
+                            text = "Periodo",
+                            style = MaterialTheme.typography.labelLarge,
+                            modifier = Modifier.padding(MaterialTheme.spacing.medium)
+                        )
                     }
                 }
             }
+            Text(text = "Scegli data inizio", style = MaterialTheme.typography.headlineMedium)
+            CustomDateButton(
+                date = viewModel.startDate.value,
+                onDateSelected = { date -> viewModel.startDate.value = fromDateToLocalDate(date) })
+            Text(
+                text = "Scegli data fine (opzionale)",
+                style = MaterialTheme.typography.headlineMedium
+            )
+            CustomDateButton(
+                date = viewModel.endDate.value,
+                onDateSelected = { date -> viewModel.endDate.value = fromDateToLocalDate(date) })
         }
     }
 }
--- app/src/main/java/com/example/eplan/presentation/ui/vacationRequest/VacationRequestViewModel.kt
+++ app/src/main/java/com/example/eplan/presentation/ui/vacationRequest/VacationRequestViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.eplan.presentation.ui.vacationRequest
 
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.example.eplan.presentation.ui.EplanViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import java.time.LocalDate
 import javax.inject.Inject
@@ -13,9 +16,12 @@ class VacationRequestViewModel
 @Inject
 constructor(
     savedStateHandle: SavedStateHandle
-) : ViewModel() {
-    val startDate: MutableState<LocalDate?> = mutableStateOf(null)
-    val endDate: MutableState<LocalDate?> = mutableStateOf(null)
+) : EplanViewModel() {
+
+    var singleDayVacancy by mutableStateOf(true)
+    val startDate: MutableState<LocalDate> = mutableStateOf(LocalDate.now())
+    val endDate: MutableState<LocalDate> = mutableStateOf(LocalDate.now())
+    val selectedDate: MutableState<LocalDate?> = mutableStateOf(null)
 
     init {
 
