c154a35007ee083642fac0ad698519e9df738aa7
Nikolas Guillen Leon <n.guillen@cosmobile.com>
2023-08-28 11:02:30 +0200

    fix comportamento schermata login

--- app/src/main/java/com/example/eplan/presentation/ui/login/LoginScreen.kt
+++ app/src/main/java/com/example/eplan/presentation/ui/login/LoginScreen.kt
@@ -2,16 +2,21 @@ package com.example.eplan.presentation.ui.login
 
 import android.app.Activity
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
@@ -23,7 +28,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -37,6 +41,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -44,7 +50,6 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.window.Dialog
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.eplan.R
 import com.example.eplan.presentation.util.spacing
 
@@ -52,7 +57,7 @@ import com.example.eplan.presentation.util.spacing
 @ExperimentalMaterial3Api
 @Composable
 fun LoginScreen(
-    viewModel: LoginViewModel = hiltViewModel(),
+    viewModel: LoginViewModel,
     onLoginAttempted: () -> Unit,
     onSuccessfulLogin: () -> Unit
 ) {
@@ -61,6 +66,8 @@ fun LoginScreen(
     val loading = viewModel.loading
     val activity = LocalContext.current as Activity
     var passwordVisibility by remember { mutableStateOf(false) }
+    val focusManager = LocalFocusManager.current
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     LaunchedEffect(key1 = true) {
         viewModel.successfulLoginAttempt.collect {
@@ -68,115 +75,140 @@ fun LoginScreen(
         }
     }
 
-    Scaffold { paddingValues ->
-        BackHandler(enabled = true) {
-            activity.finish()
+    BackHandler(enabled = true) {
+        activity.finish()
+    }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .imePadding()
+            .clickable(
+                enabled = true,
+                onClick = {
+                    focusManager.clearFocus()
+                    keyboardController?.hide()
+                },
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            )
+    ) {
+        if (!successfulLogin) {
+            Text(text = viewModel.message.value, color = Color.Red)
         }
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
+
+        Spacer(modifier = Modifier.height(MaterialTheme.spacing.small))
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.extraSmall),
+            modifier = Modifier.fillMaxWidth(.8f)
         ) {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.small),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .padding(MaterialTheme.spacing.large)
-                    .fillMaxWidth()
-            ) {
-                if (!successfulLogin) {
-                    Text(text = viewModel.message.value, color = Color.Red)
-                }
-                Column(verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.extraSmall)) {
-                    OutlinedTextField(
-                        value = viewModel.username.value,
-                        onValueChange = { viewModel.username.value = it },
-                        placeholder = { Text(text = stringResource(R.string.username)) },
-                        isError = viewModel.usernameError.value != null,
-                        singleLine = true,
-                        keyboardOptions = KeyboardOptions(
-                            imeAction = ImeAction.Next,
-                            keyboardType = KeyboardType.Email
-                        ),
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    if (viewModel.usernameError.value != null) {
-                        Text(
-                            text = stringResource(R.string.campo_vuoto),
-                            color = MaterialTheme.colorScheme.error,
-                            style = MaterialTheme.typography.labelMedium,
-                            textAlign = TextAlign.End,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                    }
-                }
-
-                Column(verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.extraSmall)) {
-                    OutlinedTextField(
-                        value = viewModel.password.value,
-                        onValueChange = { viewModel.password.value = it },
-                        placeholder = { Text(text = stringResource(R.string.password)) },
-                        visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
-                        keyboardOptions = KeyboardOptions(
-                            keyboardType = KeyboardType.Password,
-                            imeAction = ImeAction.Done
-                        ),
-                        trailingIcon = {
-                            val image =
-                                if (passwordVisibility) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
-
-                            val description =
-                                if (passwordVisibility) stringResource(R.string.nascondi_password)
-                                else stringResource(R.string.mostra_password)
-
-                            IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
-                                Icon(imageVector = image, description)
-                            }
-                        },
-                        isError = viewModel.passwordError.value != null,
-                        singleLine = true,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    if (viewModel.passwordError.value != null) {
-                        Text(
-                            text = stringResource(R.string.campo_vuoto),
-                            color = MaterialTheme.colorScheme.error,
-                            style = MaterialTheme.typography.labelMedium,
-                            textAlign = TextAlign.End,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                    }
-                }
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Checkbox(
-                        checked = viewModel.getAutoLogin(),
-                        onCheckedChange = { viewModel.setAutoLogin(!viewModel.getAutoLogin()) }
-                    )
-                    Text(text = stringResource(R.string.resta_collegato))
-                }
-                Button(onClick = {
-                    onLoginAttempted()
-                }) {
-                    Text(text = stringResource(R.string.login))
-                }
-
+            OutlinedTextField(
+                value = viewModel.username.value,
+                onValueChange = { viewModel.username.value = it },
+                placeholder = { Text(text = stringResource(R.string.username)) },
+                isError = viewModel.usernameError.value != null,
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next,
+                    keyboardType = KeyboardType.Email
+                ),
+                modifier = Modifier.fillMaxWidth()
+            )
+            if (viewModel.usernameError.value != null) {
+                Text(
+                    text = stringResource(R.string.campo_vuoto),
+                    color = MaterialTheme.colorScheme.error,
+                    style = MaterialTheme.typography.labelMedium,
+                    textAlign = TextAlign.End,
+                    modifier = Modifier.fillMaxWidth()
+                )
             }
         }
-        if (loading.value) {
-            Dialog(onDismissRequest = {}) {
-                Surface(shape = MaterialTheme.shapes.medium) {
-                    Column(
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier.padding(all = MaterialTheme.spacing.large)
-                    ) {
-                        CircularProgressIndicator()
-                        Spacer(modifier = Modifier.size(MaterialTheme.spacing.medium))
-                        Text(text = stringResource(R.string.accesso_in_corso))
+
+        Spacer(modifier = Modifier.height(MaterialTheme.spacing.medium))
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.extraSmall),
+            modifier = Modifier.fillMaxWidth(.8f)
+        ) {
+            OutlinedTextField(
+                value = viewModel.password.value,
+                onValueChange = { viewModel.password.value = it },
+                placeholder = { Text(text = stringResource(R.string.password)) },
+                visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Done
+                ),
+                trailingIcon = {
+                    val image =
+                        if (passwordVisibility) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+
+                    val description =
+                        if (passwordVisibility) stringResource(R.string.nascondi_password)
+                        else stringResource(R.string.mostra_password)
+
+                    IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
+                        Icon(imageVector = image, description)
                     }
+                },
+                isError = viewModel.passwordError.value != null,
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+            if (viewModel.passwordError.value != null) {
+                Text(
+                    text = stringResource(R.string.campo_vuoto),
+                    color = MaterialTheme.colorScheme.error,
+                    style = MaterialTheme.typography.labelMedium,
+                    textAlign = TextAlign.End,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(MaterialTheme.spacing.small))
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(
+                checked = viewModel.getAutoLogin(),
+                onCheckedChange = { viewModel.setAutoLogin(!viewModel.getAutoLogin()) }
+            )
+            Text(
+                text = stringResource(R.string.resta_collegato),
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        }
+
+        Spacer(modifier = Modifier.height(MaterialTheme.spacing.small))
+
+        Button(
+            onClick = {
+                onLoginAttempted()
+            }
+        ) {
+            Text(text = stringResource(R.string.login))
+        }
+
+    }
+
+    if (loading.value) {
+        Dialog(onDismissRequest = {}) {
+            Surface(shape = MaterialTheme.shapes.medium) {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.padding(all = MaterialTheme.spacing.large)
+                ) {
+                    CircularProgressIndicator()
+                    Spacer(modifier = Modifier.size(MaterialTheme.spacing.medium))
+                    Text(text = stringResource(R.string.accesso_in_corso))
                 }
             }
         }
